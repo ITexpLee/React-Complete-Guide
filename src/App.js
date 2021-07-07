7fb795e44c407c204447f2ba8b23d38545d62b05
Mikel8Lee <connerrobert927@gmail.com>
2021-07-08 04:14:12 +0530

    Added list data dynamically using state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //Importing custom components in tree ascending order
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  //Fetching Data Object for expenses
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+//Fetching Data Object for expenses (Initial Expense default State)
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
+const App = () => {
+  // Using State to keep track of expenses
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
   //Creating a user defined function for event to get Expense
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    // Use the data of user created expense to store in state
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
@@ -38,7 +43,7 @@ const App = () => {
       {/* We will pass all data to be required in expense item component as attributes*/}
       {/* We can pass the entire array of objects as attributes too*/}
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses item={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -21,26 +21,14 @@ const Expenses = (props) => {
     <div>
     <Card className="expenses">
     <ExpensesFilter onFilterChange={filterChangeHandler} year={filteredYear}/>
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      />
+      {/* Printing out our code by props.array to create ExpenseItem */}
+      {props.items.map(expense => 
+      <ExpenseItem 
+      key={expense.id}
+      title={expense.title} 
+      amount={expense.amount} 
+      date={expense.date}/>
+      )};
     </Card>
     </div>
   );
